fd2b0aa9807c6e29a311b8ecdaf5386b4906c996
abhiroop1999 <abhiroopsomisetty@gmail.com>
2025-06-08 10:24:24 -0500

    Delete routes.py

--- routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from models import VendorRequest
-from utils.vendor_agent import discover_and_score_vendors
-
-router = APIRouter()
-
-@app.post("/vendor-agent")
-def vendor_agent_endpoint(request: VendorAgentRequest):
-    return discover_and_score_vendors(request.product, request.quantity, request.location)
