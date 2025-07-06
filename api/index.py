42c05742bbfb1d7f4001d02a0668fc0a27286047
abhiroop1999 <abhiroopsomisetty@gmail.com>
2025-07-06 16:40:20 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
 # /api/index.py
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from fastapi import FastAPI
-from ..routes import router # Note the two dots to go up one directory
+from routes import router
 
 app = FastAPI()
 
